e8f8a525e8b8b76d4fe2c2f04a766db2afeaab25
naiaragabriela <naigabi31@gmail.com>
2023-03-05 19:04:27 -0300

    cálculo media dos numeros usando funções

---
+++ src/ProgramaMediaDoisNumeros/ProgramaMediaDoisNumeros/Program.cs
@@ -0,0 +1,22 @@
+﻿float n1, n2;
+
+float PrimeiroNumero()
+{
+    Console.Write("Digite o primeiro número da média: ");
+    return float.Parse(Console.ReadLine());
+}
+float SegundoNumero()
+{
+    Console.Write("Digite o segundo número da média: ");
+    return float.Parse(Console.ReadLine());
+}
+
+
+
+void MediaNumeros()
+{
+    Console.WriteLine("A média dos números digitados é: " + ((PrimeiroNumero() + SegundoNumero())/2).ToString("F2"));
+    Console.ReadKey();
+}
+
+MediaNumeros();
